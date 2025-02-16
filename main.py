a9961cc53274d89cb09d33630b03bda7eef64f80
Marcus Andrade <profmarcusandrade@gmail.com>
2025-02-15 22:46:17 -0300

    instalação das libs e inicio da arquitetura do projeto

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def Main():
+    st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="collapsed", menu_items=None)
+    st.text("Controlador CLP - Micro 820 e Micro 850 - Python")
+
+if __name__ == "__main__":
+    Main()
